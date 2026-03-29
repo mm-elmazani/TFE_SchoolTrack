bd49022b808af72cd6c7b7145f42a761ea921cad
Mohamed Mokhtar El Mazani <m.elmazani@students.ephec.be>
2026-03-29 18:40:01 +0200

    [MAJ] - URL API prod

--- flutter_teacher_app/lib/core/constants.dart
+++ flutter_teacher_app/lib/core/constants.dart
@@ -5,7 +5,8 @@ library;
 /// En dev : adresse IP locale de la machine qui fait tourner le backend.
 /// En prod : URL publique via Traefik (ex: https://api.schooltrack.be).
 
-const String kApiBaseUrl = 'http://192.168.1.2:8000'; /// ou 'http://192.168.1.2:8000'; 'http://192.168.1.18:8000';
+const String kApiBaseUrl = 'https://api.schooltrack.yourschool.be';
+/// Dev : 'http://192.168.1.2:8000' ou 'http://192.168.1.18:8000'
 
 /// Durée de validité du cache offline (7 jours en millisecondes).
 const int kOfflineCacheDurationMs = 7 * 24 * 60 * 60 * 1000;
